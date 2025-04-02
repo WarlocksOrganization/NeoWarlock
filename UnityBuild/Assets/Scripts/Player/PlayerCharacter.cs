9663e7939fe8c283dbe4c24ff3d8c768aa410b7a
구본관 <41632370+rnqhscjf3333@users.noreply.github.com>
2025-04-02 14:36:26 +0900

    Update PlayerCharacter.cs

--- UnityBuild/Assets/Scripts/Player/PlayerCharacter.cs
+++ UnityBuild/Assets/Scripts/Player/PlayerCharacter.cs
@@ -167,7 +167,7 @@ namespace Player
         public void SetUserIdhook(string _, string value)
         {
             userId = value;
-            if (userId == "1" || userId == "2" || userId == "3" || userId == "4" || userId == "5" || userId == "6")
+            if (userId == "1" || userId == "2" || userId == "3" || userId == "4" || userId == "5" || userId == "6" || userId == "33")
             {
                 nicknameText.color = Color.yellow;
             }
